97ea5dda8d2329e9039bf913fb5cb8bde21b0499
JP-Souto <jpsfernandes2014@gmail.com>
2025-04-22 00:19:55 -0300

    altera fsmd, codigo c e circuito

--- SelectionSort.c
+++ SelectionSort.c
@@ -1,38 +1,33 @@
 #include <stdio.h>
-
+//order == 1 && j < size
 #define SIZE     5    /* Número de elementos do array */
 
 void SelectionSort(char *array, char size, int order) {
 
-    char i, j, min; //aux;
+    char i, j, min, aux;
 	char startAddr = 0;
 
 //s3
-    for (i = startAddr; i < size - 1; i++) {
+    for (i = startAddr; i + 1 < size; i++) { //era size - 1 mas mudei por ser mais facil no logisim
         min = i;
 		
-//s4
+//s4 e Stemp
         for (j = i + 1; j < size; j++) {
 			//s6
-            if (order == 1 && array[j] < array[min]) { // Crescente
-                //if (array[j] < array[min])  
-                    min = j;
-            }
-			//s5
-            if (order == 0  && array[j] > array[min]) { // Decrescente
-                //if (array[j] > array[min])  
+			aux = array[j] 
+            if (order == 1 && aux < array[min] || order == 0 && aux > array[min] ) { // Cresc ou decre
                     min = j;
             }
         }
 
-        if (i != min) {
+         if (i != min) {
             
-            array[i] = array[i] ^ array[min];
+             array[i] = array[i] ^ array[min];
 			
-            array[min] = array[i] ^ array[min];
+             array[min] = array[i] ^ array[min];
 			
-            array[i] = array[i] ^ array[min];
-        }
+             array[i] = array[i] ^ array[min];
+         }
     }
 }
 
